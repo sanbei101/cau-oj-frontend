1b2d1a4782564d489bfd0973b9e551b63377f519
sanbei101 <sanbei101@outlook.com>
2025-01-17 10:38:25 +0800

    优化结果对话框组件,增加加载状态,动态标题,结果图标

--- src/views/components/Submission/ResultDialog.vue
+++ src/views/components/Submission/ResultDialog.vue
@@ -1,21 +1,47 @@
 <template>
-  <n-result title="OJ 代码评测结果" status="success" size="large">
-    <template #default>
-      <n-data-table :columns="columns" :data="tableData.results" size="large" :pagination="false" />
-    </template>
-  </n-result>
+  <n-spin :show="loading">
+    <n-result :title="Title" :status="Status" size="large">
+      <template #default>
+        <n-data-table :columns="columns" :data="tableData.results" :loading="loading" :pagination="false" :empty="'暂无数据'" />
+      </template>
+    </n-result>
+  </n-spin>
 </template>
 
 <script setup lang="ts">
-import { ref, onBeforeMount } from 'vue';
-import { NResult, NDataTable, type DataTableColumns } from 'naive-ui';
+import { ref, computed, onMounted } from 'vue';
+import { NResult, NDataTable, NSpin, type DataTableColumns, type ResultProps, useMessage } from 'naive-ui';
 import { ErrorMessage, JudgeResult, SingleResult, SubmitData } from '@/api/type';
 import { JudgeApi } from '@/api/request';
 import { useStore } from '@/store';
+const message = useMessage();
 const store = useStore();
+const loading = ref<boolean>(false);
 const props = defineProps<{
   submitData: SubmitData;
 }>();
+
+const Status = computed<ResultProps['status']>(() => {
+  const totalCount = tableData.value.count;
+  if (totalCount === 0) return 'info';
+  const successCount = tableData.value.results.filter((result) => result.is_success).length;
+  if (successCount === totalCount) {
+    message.success('全部通过');
+    return 'success';
+  }
+  if (successCount === 0) {
+    message.error('失败了哦');
+    return 'error';
+  } else {
+    message.warning('部分通过');
+    return 'warning';
+  }
+});
+
+const Title = computed<string>(() => {
+  if (loading.value) return '评测中...';
+  return '评测结果';
+});
 
 const columns: DataTableColumns<SingleResult> = [
   {
@@ -34,21 +60,30 @@ const columns: DataTableColumns<SingleResult> = [
   },
   {
     title: '期望输出',
-    key: 'expected_output'
+    key: 'expected_output',
+    ellipsis: {
+      tooltip: true
+    }
   },
   {
     title: '实际输出',
-    key: 'actual_output'
+    key: 'actual_output',
+    ellipsis: {
+      tooltip: true
+    }
   }
 ];
 
 const tableData = ref<JudgeResult>({ count: 0, results: [] });
-onBeforeMount(() => {
-  console.log(props.submitData);
+
+onMounted(() => {
+  console.log(props.submitData.sourceCode);
+  loading.value = true;
   JudgeApi.submit(props.submitData)
     .then((data) => {
       console.log(data);
       tableData.value = data;
+      loading.value = false;
     })
     .catch((err: ErrorMessage) => {
       store.app.setError(err);
